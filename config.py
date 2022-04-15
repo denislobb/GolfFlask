b68a8dd944973a49e08bbc75c9a3a30969f9986b
denislobb <31692260+denislobb@users.noreply.github.com>
2022-04-15 13:24:34 +1000

    7a - converted to Blueprint and refactored

---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+
+basedir = Path(__file__).parent.resolve()
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    GOLFER_MAIL_SUBJECT_PREFIX = '[Golfer]'
+    GOLFER_MAIL_SENDER = 'Golfer Admin <Golfer@example.com>'
+    GOLFER_ADMIN = os.environ.get('Golfer_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + str(Path(basedir, 'data-dev.sqlite'))
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite://'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + str(Path(basedir, 'data.sqlite'))
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
