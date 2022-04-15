75070cec44215ebed601b28ffb7c3cadd33250f0
denislobb <31692260+denislobb@users.noreply.github.com>
2022-04-15 10:39:35 +1000

    6b - email functions added

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
+import os
+from datetime import datetime
+from pathlib import Path
+from threading import Thread
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -6,9 +10,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from datetime import datetime
-from pathlib import Path
-
+from flask_mail import Mail, Message
 
 basedir = Path(__file__).parent.resolve()
 
@@ -17,11 +19,21 @@ app.config['SECRET_KEY'] = 'my hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + str(Path(basedir, 'dev_db.sqlite'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+app.config['GOLFER_MAIL_SUBJECT_PREFIX'] = '[Golfer]'
+app.config['GOLFER_MAIL_SENDER'] = 'Golfer Admin <golfer@google.com'
+app.config['GOLFER_ADMIN'] = os.environ.get('GOLFER_ADMIN')
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 class Role(db.Model):
@@ -42,6 +54,21 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}'
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['GOLFER_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['GOLFER_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
 
 
 class NameForm(FlaskForm):
@@ -74,6 +101,9 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['GOLFER_ADMIN']:
+                send_mail(app.config['GOLFER_ADMIN'], 'New User',
+                          'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
